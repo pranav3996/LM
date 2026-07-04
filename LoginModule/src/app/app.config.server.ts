7d4bea9f1a96e6254978e1401af7e8d4ec7b35b3
pranav3996 <54428844+pranav3996@users.noreply.github.com>
2026-07-04 11:22:31 +0530

    v20

--- LoginModule/src/app/app.config.server.ts
+++ LoginModule/src/app/app.config.server.ts
@@ -1,15 +1,11 @@
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
 
 import { serverRoutes } from './app.routes.server';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRouting(serverRoutes)
-  ]
+  providers: [provideServerRendering(withRoutes(serverRoutes))]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
